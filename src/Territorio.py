263a11f8eef976d9eed2ab0d8b51f4258b9d0e7b
Fernanda <fernandafelix789@gmail.com>
2024-09-11 20:02:41 -0300

    correcao

--- src/Territorio.py
+++ src/Territorio.py
@@ -5,42 +5,42 @@ class Territorio:
         if carta.get_tipo() != "Território":
             raise ValueError("A carta associada deve ser do tipo 'Território'.")
         
-        self._nome = carta.get_descricao()
-        self._jogador = jogador
-        self._qnt_exercitos = qnt_exercitos
-        self._vizinhos = vizinhos
+        self.nome = carta.get_descricao()
+        self.jogador = jogador
+        self.qnt_exercitos = qnt_exercitos
+        self.vizinhos = vizinhos
     
 
     def get_nome(self) -> str:
-        return self._nome
+        return self.nome
 
     def get_jogador(self) -> str:
-        return self._jogador
+        return self.jogador
 
     def get_qnt_exercitos(self) -> int:
-        return self._qnt_exercitos
+        return self.qnt_exercitos
 
     def get_vizinhos(self) -> list:
-        return self._vizinhos
+        return self.vizinhos
     
     def set_nome(self, nome: str):
-        self._nome = nome
+        self.nome = nome
 
     def set_jogador(self, jogador: str):
-        self._jogador = jogador
+        self.jogador = jogador
 
     def set_qnt_exercitos(self, qnt: int):
         if qnt < 0:
             raise ValueError("A quantidade de exércitos não pode ser negativa.")
-        self._qnt_exercitos = qnt
+        self.qnt_exercitos = qnt
 
     def set_vizinhos(self, vizinhos: list):
-        self._vizinhos = vizinhos
+        self.vizinhos = vizinhos
     
     def adicionar_exercitos(self, qnt: int):
-        self._qnt_exercitos += qnt
+        self.qnt_exercitos += qnt
     
     def remover_exercitos(self, qnt: int):
-        if qnt > self._qnt_exercitos:
+        if qnt > self.qnt_exercitos:
             raise ValueError("Não há tropas suficientes para remover.")
-        self._qnt_exercitos -= qnt
+        self.qnt_exercitos -= qnt
